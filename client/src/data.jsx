7edf2bd69a537bf90802235badff471307a4e23f
Vipin019 <vkpmail019@gmail0com>
2023-10-29 13:10:55 +0530

    ESP folder structure is changed

--- client/src/data.jsx
+++ client/src/data.jsx
@@ -27,21 +27,19 @@ const Data = () => {
   }, []);
   return (
     <div>
-      {/* <h2>Smart Meter Reading</h2> */}
-      <h1><u>Smart Meter Dashboard</u></h1>
+      <h1>
+        <u>Smart Meter Dashboard</u>
+      </h1>
       {updatedData && (
-        // <div>
-        //   <p>Voltage: {updatedData.vrms} and Currrent: {updatedData.irms}</p>
-        //   <p>Real Power: {updatedData.realPower} and Apparent Powet: {updatedData.apparentPower}  Kwh: {updatedData.kwh}</p>
-
-        // </div>
-
         // contributed by Prince Kumar (20UEE05)
         <div className="meter-container">
           <MeterCard title="Voltage (V)" value={updatedData.vrms} />
           <MeterCard title="Current (A)" value={updatedData.irms} />
           <MeterCard title="Real Power (W)" value={updatedData.realPower} />
-          <MeterCard title="Apparent Power (VA)" value={updatedData.apparentPower} />
+          <MeterCard
+            title="Apparent Power (VA)"
+            value={updatedData.apparentPower}
+          />
           <MeterCard title="Energy Consume (KWh)" value={updatedData.kwh} />
         </div>
       )}
@@ -50,36 +48,3 @@ const Data = () => {
 };
 
 export default Data;
-
-// import React, { useState, useEffect } from 'react';
-// import WebSocket from 'react-websocket';
-
-// function App() {
-//   const [pinsData, setPinsData] = useState({pin1: 0, pin2: 0});
-
-//   const handleData = (data) => {
-//     // Data received from WebSocket
-//     const jsonData = JSON.parse(data);
-//     setPinsData(jsonData);
-//   };
-
-//   useEffect(() => {
-//     // Connect to WebSocket
-//     const ws = new WebSocket('ws://YOUR_ESP32_IP:81/'); // Replace with your ESP32 IP
-
-//     return () => {
-//       // Clean up WebSocket connection when component unmounts
-//       ws.close();
-//     };
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <h1>Pins Data</h1>
-//       <p>Pin 1: {pinsData.pin1}</p>
-//       <p>Pin 2: {pinsData.pin2}</p>
-//     </div>
-//   );
-// }
-
-// export default App;
